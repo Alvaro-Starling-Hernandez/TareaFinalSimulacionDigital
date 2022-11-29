92e05f474b699d7e08b3ea8185ce52d546f5b767
PrandiFabiel <fabiel098@gmail.com>
2022-11-29 00:14:35 -0400

    Ya se mueven las imagenes.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -81,6 +81,8 @@
             this.maizP2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Dron = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.maizGG8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.maizGG7)).BeginInit();
@@ -124,6 +126,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.maizP2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dron)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -149,7 +152,7 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(380, 103);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(87, 15);
+            this.label9.Size = new System.Drawing.Size(76, 13);
             this.label9.TabIndex = 26;
             this.label9.Text = "Agua utilizada:";
             // 
@@ -158,7 +161,7 @@
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(380, 81);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(190, 15);
+            this.label8.Size = new System.Drawing.Size(160, 13);
             this.label8.TabIndex = 25;
             this.label8.Text = "Cantidad de semillas sembradas:";
             // 
@@ -167,7 +170,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(380, 58);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(119, 15);
+            this.label7.Size = new System.Drawing.Size(104, 13);
             this.label7.TabIndex = 24;
             this.label7.Text = "Terreno recuperado:";
             // 
@@ -176,7 +179,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(380, 36);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(130, 15);
+            this.label6.Size = new System.Drawing.Size(110, 13);
             this.label6.TabIndex = 23;
             this.label6.Text = "Semillas recuperadas:";
             // 
@@ -185,7 +188,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(14, 104);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(132, 15);
+            this.label5.Size = new System.Drawing.Size(115, 13);
             this.label5.TabIndex = 22;
             this.label5.Text = "Porcentaje recuperado";
             // 
@@ -194,7 +197,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(14, 81);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(189, 15);
+            this.label4.Size = new System.Drawing.Size(163, 13);
             this.label4.TabIndex = 21;
             this.label4.Text = "Porcentaje de semillas con dano:";
             // 
@@ -203,7 +206,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(14, 57);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(189, 15);
+            this.label3.Size = new System.Drawing.Size(166, 13);
             this.label3.TabIndex = 20;
             this.label3.Text = "Porcentaje de terreno con abono:";
             // 
@@ -212,7 +215,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(14, 34);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 15);
+            this.label2.Size = new System.Drawing.Size(99, 13);
             this.label2.TabIndex = 19;
             this.label2.Text = "Terreno sembrado: ";
             // 
@@ -222,7 +225,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(272, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 17);
+            this.label1.Size = new System.Drawing.Size(124, 13);
             this.label1.TabIndex = 18;
             this.label1.Text = "Datos en tiempo real";
             // 
@@ -686,6 +689,26 @@
             this.pictureBox2.TabIndex = 10;
             this.pictureBox2.TabStop = false;
             // 
+            // Dron
+            // 
+            this.Dron.Image = ((System.Drawing.Image)(resources.GetObject("Dron.Image")));
+            this.Dron.Location = new System.Drawing.Point(65, 60);
+            this.Dron.Name = "Dron";
+            this.Dron.Size = new System.Drawing.Size(100, 50);
+            this.Dron.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dron.TabIndex = 53;
+            this.Dron.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 54;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form1
             // 
             this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
@@ -693,6 +716,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1278, 703);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Dron);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.maizGG8);
             this.Controls.Add(this.maizGG7);
@@ -784,6 +809,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.maizP2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dron)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -841,6 +867,8 @@
         private System.Windows.Forms.PictureBox maizGG6;
         private System.Windows.Forms.PictureBox maizGG7;
         private System.Windows.Forms.PictureBox maizGG8;
+        private System.Windows.Forms.PictureBox Dron;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace TareaFinalSimulacionDigital
@@ -65,6 +66,34 @@ namespace TareaFinalSimulacionDigital
         private void hueco7_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void eventoTimer(object sender, ElapsedEventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            moveImage(Dron.Left, Dron.Top);
+        }
+
+        private void moveImage(int x, int y)
+        {
+            if (x < this.Width - Dron.Width)
+            {
+                Dron.Left = x + 20;
+            }
+            else if (y < this.Height - Dron.Height)
+            {
+                Dron.Top = y + 20;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            System.Timers.Timer miTimer = new System.Timers.Timer();
+            miTimer.Elapsed += new ElapsedEventHandler(eventoTimer);
+            miTimer.Interval = 200;
+            miTimer.Enabled = true;
+            GC.KeepAlive(miTimer);
         }
     }
 }
